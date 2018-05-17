dc7f9a4ab147988a72993ae227d67b8efef1049f
r-cox <Cox_Russell@bah.com>
2018-05-16 23:21:06 -0400

    made synthesizing arbitrary bs easier. added default arbitrary bs

--- synthesize.py
+++ synthesize.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 # /usr/bin/python2
 '''
-By kyubyong park. kbpark.linguist@gmail.com.
+original code By kyubyong park. kbpark.linguist@gmail.com.
 https://www.github.com/kyubyong/dc_tts
+
+adapted by Russ Cox for easy text2speech conversion
 '''
 
 from __future__ import print_function
@@ -14,31 +16,72 @@ import numpy as np
 import tensorflow as tf
 from train import Graph
 from utils import *
-from data_load import load_data
+from data_load import load_data, load_vocab, text_normalize
 from scipy.io.wavfile import write
 from tqdm import tqdm
+import time
 
-def synthesize():
-    # Load data
-    L = load_data("synthesize")
 
-    # Load graph
-    g = Graph(mode="synthesize"); print("Graph loaded")
+class Synth:
 
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
+    def __init__(self, log_dir="log_dir", sample_dir="samples"):
+        self._sess_loaded = False
+        self.log_dir = log_dir
+        self.sample_dir = sample_dir
+        char2idx, idx2char = load_vocab()
+        self.char2idx = char2idx
+        self.idx2char = idx2char
+        self.g = Graph(mode="synthesize")
+        print("Graph loaded")
+        self.load_session()
+
+
+    def prep_text(self, text):
+        lines = text.split('\n')
+        sents = [text_normalize(line).strip() + "E" for line in lines]
+        texts = np.zeros((len(sents), hp.max_N), np.int32)
+        for i, sent in enumerate(sents):
+            texts[i, :len(sent)] = [self.char2idx[char] for char in sent]
+        return texts
+
+    def load_session(self):
+        self.sess = tf.Session()
+        self.sess.run(tf.global_variables_initializer())
 
         # Restore parameters
         var_list = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'Text2Mel')
         saver1 = tf.train.Saver(var_list=var_list)
-        saver1.restore(sess, tf.train.latest_checkpoint(hp.logdir + "-1"))
+        saver1.restore(self.sess,
+                       tf.train.latest_checkpoint(os.path.join(self.log_dir, "{}-1".format("LJ01"))))
         print("Text2Mel Restored!")
 
         var_list = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'SSRN') + \
                    tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, 'gs')
         saver2 = tf.train.Saver(var_list=var_list)
-        saver2.restore(sess, tf.train.latest_checkpoint(hp.logdir + "-2"))
+        saver2.restore(self.sess,
+                       tf.train.latest_checkpoint(os.path.join(self.log_dir, "{}-2".format("LJ01"))))
         print("SSRN Restored!")
+        self._sess_loaded = True
+
+    def close_session(self):
+        self.sess.close()
+        self._sess_loaded = False
+        print('closed session')
+
+
+    def synthesize(self, text):
+        if not os.path.exists(self.sample_dir): os.makedirs(self.sample_dir)
+
+        if not self._sess_loaded:
+            self.load_session()
+
+        text = text.strip().lower()
+        fn = os.path.join(self.sample_dir, "{}.wav".format(str(hash(text)).replace('-','_')))
+
+        if os.path.isfile(fn):
+            return fn
+        start = time.time()
+        L = self.prep_text(text)
 
         # Feed Forward
         ## mel
@@ -46,25 +89,42 @@ def synthesize():
         prev_max_attentions = np.zeros((len(L),), np.int32)
         for j in tqdm(range(hp.max_T)):
             _gs, _Y, _max_attentions, _alignments = \
-                sess.run([g.global_step, g.Y, g.max_attentions, g.alignments],
-                         {g.L: L,
-                          g.mels: Y,
-                          g.prev_max_attentions: prev_max_attentions})
+                self.sess.run([self.g.global_step, self.g.Y, self.g.max_attentions, self.g.alignments],
+                         {self.g.L                  : L,
+                          self.g.mels               : Y,
+                          self.g.prev_max_attentions: prev_max_attentions})
             Y[:, j, :] = _Y[:, j, :]
             prev_max_attentions = _max_attentions[:, j]
 
         # Get magnitude
-        Z = sess.run(g.Z, {g.Y: Y})
+        Z = self.sess.run(self.g.Z, {self.g.Y: Y})
 
         # Generate wav files
-        if not os.path.exists(hp.sampledir): os.makedirs(hp.sampledir)
+
         for i, mag in enumerate(Z):
-            print("Working on file", i+1)
+            print("Working on file {}:{}".format(fn, i))
             wav = spectrogram2wav(mag)
-            write(hp.sampledir + "/{}.wav".format(i+1), hp.sr, wav)
+            write(fn, hp.sr, wav)
+        stop = time.time()
+        print("{} seconds elapsed".format(stop-start))
+
+
+
 
 if __name__ == '__main__':
-    synthesize()
-    print("Done")
+    synth = Synth()
+
+
+    import json
+
+    with open(os.path.join("data", "common.json"), 'r') as f:
+        data = json.loads(f.read())
+    for k in data:
+        for sub_k in data[k]:
+            for quote in data[k][sub_k]:
+                synth.synthesize(quote)
+
+    synth.close_session()
+
 
 
